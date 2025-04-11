072814f82d518387e07045fb5229436996989533
jaivikrathod <jaivikrathod7@gmail.com>
2025-04-12 00:05:29 +0530

    fix

--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { AppContent, AppSidebar, AppFooter, AppHeader } from '../components/index'
-
+import PrivateRoute from '../PrivateRoute'
 const DefaultLayout = () => {
   return (
     <PrivateRoute>
